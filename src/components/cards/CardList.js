fb9a95d38602238558625d9c97e0215fcdf8c360
Ronan Morris <ronanmorris11@gmail.com>
2019-05-13 15:34:33 +1000

    Pointer cursor on load more

--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -11,6 +11,7 @@ const Card = styled.div`
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   &:hover {
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    cursor: pointer;
   }
   -moz-user-select: none;
   -website-user-select: none;
