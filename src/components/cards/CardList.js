faf4ca91e5d2977c03818fd316fafad5f32c5d2e
Ronan Morris <ronanmorris11@gmail.com>
2019-05-13 12:27:26 +1000

    Lazy Load test

--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -473,10 +473,9 @@ export default class CardList extends Component {
             
           
           <div className="row">
-          
+          <LazyLoadComponent>
           {filteredCards.map(card => (
              <div key={"a" + card.index} className={"width-" + this.state.cardsPerRow.toString()}>
-              <LazyLoadComponent>
                 <CardCard
                   key={card.index}
                   index={card.index}
@@ -484,9 +483,9 @@ export default class CardList extends Component {
                   url={card.url}
                   columns={this.state.cardsPerRow}
                 />
-              </LazyLoadComponent>
             </div>
             ))}
+            </LazyLoadComponent>
           </div>
         </div>
         ) : (
