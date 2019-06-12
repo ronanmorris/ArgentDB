bd191e8b6236f5d260d1c156129f8252dd72d787
Ronan Morris <ronanmorris11@gmail.com>
2019-06-13 09:51:19 +1000

    modal animation test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { ModalContainer, ModalLink } from 'react-router-modal';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { ModalContainer, ModalLink } from "react-router-modal";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 import NavBar from "./components/layout/NavBar";
-import Dashboard from './components/layout/Dashboard';
-import Cards from './components/cards/Cards.js';
+import Dashboard from "./components/layout/Dashboard";
+import Cards from "./components/cards/Cards.js";
 
-import backgroundImage from './pattern.png';
-import DeckBuilderButton from './components/layout/DeckBuilderButton';
-import DeckBuilder from './components/cards/DeckBuilder';
-
+import backgroundImage from "./pattern.png";
+import DeckBuilderButton from "./components/layout/DeckBuilderButton";
+import DeckBuilder from "./components/cards/DeckBuilder";
 
 class App extends Component {
   render() {
@@ -22,7 +20,10 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <div className="content">
-            <div className="App" style={{background: `url(${backgroundImage})`}}>
+            <div
+              className="App"
+              style={{ background: `url(${backgroundImage})` }}
+            >
               <Route exact path="/" component={NavBar} />
               <div className="container">
                 <Route path="/" component={Dashboard} />
@@ -32,14 +33,27 @@ class App extends Component {
             </div>
           </div>
           <footer>
-            <span className="nav-link">All Images, Art and Card information are the properties and copyright of Alter Reality, LLC.</span>
-            <span className="nav-link">This is a fan made webiste. This website is in no way affiliated with, endorsed by, associated with or in any way officially connected with, Alter Reality, LLC. or any of their realated affiliates or brands</span>
+            <span className="nav-link">
+              All Images, Art and Card information are the properties and
+              copyright of Alter Reality, LLC.
+            </span>
+            <span className="nav-link">
+              This is a fan made webiste. This website is in no way affiliated
+              with, endorsed by, associated with or in any way officially
+              connected with, Alter Reality, LLC. or any of their realated
+              affiliates or brands
+            </span>
           </footer>
-          <ModalContainer />
+          <ModalContainer
+            className="card-modal"
+            modalInClassName="card-modal-in"
+            modalOutClassName="card-modal-out"
+            backdropClassName="example-backdrop"
+            backdropInClassName="example-backdrop-in"
+            backdropOutClassName="example-backdrop-out"
+          />
         </div>
-          
       </BrowserRouter>
-      
     );
   }
 }
