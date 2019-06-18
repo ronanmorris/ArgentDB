8851b5f73623aff9a5b93ea105888445b4b4bd49
Ronan Morris <ronanmorris11@gmail.com>
2019-06-18 23:14:23 +1000

    Sorts into different grids depending on card type.  Not grid swapping yet

--- src/components/cards/DeckBuilder.js
+++ src/components/cards/DeckBuilder.js
@@ -43,8 +43,8 @@ export default class DeckBuilder extends Component {
     gridChildren: [],
     championGridChildren: [],
     spiritGridChildren: [],
-    shardsGridChildren: [],
     towersGridChildren: [],
+    shardsGridChildren: [],
     sideGridChildren: [],
     counter: 0
   };
@@ -53,6 +53,21 @@ export default class DeckBuilder extends Component {
     if (!this.state.gridUp) {
       this.state.gridChildren.forEach((child, index) => {
         this.grid.getMethod("add", this.gridElement.children[index]);
+      });
+      this.state.championGridChildren.forEach((child, index) => {
+        this.gridChamp.getMethod("add", this.gridElement2.children[index]);
+      });
+      this.state.spiritGridChildren.forEach((child, index) => {
+        this.gridSpirit.getMethod("add", this.gridElement3.children[index]);
+      });
+      this.state.towersGridChildren.forEach((child, index) => {
+        this.gridTowers.getMethod("add", this.gridElement4.children[index]);
+      });
+      this.state.shardsGridChildren.forEach((child, index) => {
+        this.gridShards.getMethod("add", this.gridElement5.children[index]);
+      });
+      this.state.sideGridChildren.forEach((child, index) => {
+        this.gridSide.getMethod("add", this.gridElement6.children[index]);
       });
       this.setState({ gridUp: true });
     }
@@ -66,122 +81,225 @@ export default class DeckBuilder extends Component {
     console.log("mount");
     //setup card array for the grid if one is passed
     if (this.props.location.state) {
+      //Working copys to be set to state later
       let copyOfCardData = cardDB.cards;
       let copyOfGridChildren = [];
+      let copyOfChampionGridChildren = [];
+      let copyOfSpiritGridChildren = [];
+      let copyOfShardsGridChildren = [];
+      let copyOfTowersGridChildren = [];
+      let copyOfSideGridChildren = [];
       let x;
+
       this.state.currentDeck.forEach(card => {
         for (x = 0; x < parseInt(card.quantity, 10); x++) {
-          copyOfGridChildren.push({
-            class: "box1",
-            src: copyOfCardData[card.index].url,
-            cardIndex: card.index
-          });
+          console.log(card.type);
+          if (card.type === "Champion") {
+            if (this.state.championCard.length === 0) {
+              this.setState({
+                championCard: this.state.championCard.push(card)
+              });
+              copyOfChampionGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            } else {
+              this.setState({
+                sideDeckCards: this.state.sideDeckCards.push(card)
+              });
+              copyOfSideGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            }
+          } else if (card.type === "Spirit") {
+            if (this.state.spiritCard.length === 0) {
+              this.setState({ spiritCard: this.state.spiritCard.push(card) });
+              copyOfSpiritGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            } else {
+              this.setState({
+                sideDeckCards: this.state.sideDeckCards.push(card)
+              });
+              copyOfSideGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            }
+          } else if (card.type === "Shard") {
+            if (this.state.shardDeckCards.length < 10) {
+              this.setState({
+                shardDeckCards: this.state.shardDeckCards.push(card)
+              });
+              copyOfShardsGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            } else {
+              this.setState({
+                sideDeckCards: this.state.sideDeckCards.push(card)
+              });
+              copyOfSideGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            }
+          } else if (card.type === "Tower") {
+            if (
+              this.state.lightTowerCards.length === 0 &&
+              card.element === "Light"
+            ) {
+              this.setState({
+                lightTowerCards: this.state.lightTowerCards.push(card)
+              });
+              copyOfTowersGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            } else if (card.element === "Light") {
+              this.setState({
+                sideDeckCards: this.state.sideDeckCards.push(card)
+              });
+              copyOfSideGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            }
+            if (
+              this.state.fireTowerCards.length === 0 &&
+              card.element === "Fire"
+            ) {
+              this.setState({
+                fireTowerCards: this.state.fireTowerCards.push(card)
+              });
+              copyOfTowersGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            } else if (card.element === "Fire") {
+              this.setState({
+                sideDeckCards: this.state.sideDeckCards.push(card)
+              });
+              copyOfSideGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            }
+            if (
+              this.state.airTowerCards.length === 0 &&
+              card.element === "Air"
+            ) {
+              this.setState({
+                airTowerCards: this.state.airTowerCards.push(card)
+              });
+              copyOfTowersGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            } else if (card.element === "Air") {
+              this.setState({
+                sideDeckCards: this.state.sideDeckCards.push(card)
+              });
+              copyOfSideGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            }
+            if (
+              this.state.waterTowerCards.length === 0 &&
+              card.element === "Water"
+            ) {
+              this.setState({
+                waterTowerCards: this.state.waterTowerCards.push(card)
+              });
+              copyOfTowersGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            } else if (card.element === "Water") {
+              this.setState({
+                sideDeckCards: this.state.sideDeckCards.push(card)
+              });
+              copyOfSideGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            }
+            if (
+              this.state.darkTowerCards.length === 0 &&
+              card.element === "Dark"
+            ) {
+              this.setState({
+                darkTowerCards: this.state.darkTowerCards.push(card)
+              });
+              copyOfTowersGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            } else if (card.element === "Dark") {
+              this.setState({
+                sideDeckCards: this.state.sideDeckCards.push(card)
+              });
+              copyOfSideGridChildren.push({
+                class: "box1",
+                src: copyOfCardData[card.index].url,
+                cardIndex: card.index
+              });
+            }
+          } else if (this.state.mainDeckCards.length < 60) {
+            this.setState({
+              mainDeckCards: this.state.mainDeckCards.push(card)
+            });
+            copyOfGridChildren.push({
+              class: "box1",
+              src: copyOfCardData[card.index].url,
+              cardIndex: card.index
+            });
+          } else {
+            this.setState({
+              sideDeckCards: this.state.shardDeckCards.push(card)
+            });
+            copyOfSideGridChildren.push({
+              class: "box1",
+              src: copyOfCardData[card.index].url,
+              cardIndex: card.index
+            });
+          }
         }
       });
+
+      //update state gridChildren arrays with the values from the temporary working arrays
       this.setState({
         gridChildren: copyOfGridChildren,
+        championGridChildren: copyOfChampionGridChildren,
+        spiritGridChildren: copyOfSpiritGridChildren,
+        shardsGridChildren: copyOfShardsGridChildren,
+        towersGridChildren: copyOfTowersGridChildren,
+        sideGridChildren: copyOfSideGridChildren,
         cardData: cardDB.cards
       });
 
-      this.state.currentDeck.forEach(card => {
-        console.log(card.type);
-        if (card.type === "Champion") {
-          if (this.state.championCard.length === 0) {
-            this.setState({ championCard: this.state.championCard.push(card) });
-          } else {
-            this.setState({
-              sideDeckCards: this.state.sideDeckCards.push(card)
-            });
-          }
-        } else if (card.type === "Spirit") {
-          if (this.state.spiritCard.length === 0) {
-            this.setState({ spiritCard: this.state.spiritCard.push(card) });
-          } else {
-            this.setState({
-              sideDeckCards: this.state.sideDeckCards.push(card)
-            });
-          }
-        } else if (card.type === "Shard") {
-          if (this.state.shardDeckCards.length < 10) {
-            this.setState({
-              shardDeckCards: this.state.shardDeckCards.push(card)
-            });
-          } else {
-            this.setState({
-              sideDeckCards: this.state.sideDeckCards.push(card)
-            });
-          }
-        } else if (card.type === "Tower") {
-          if (
-            this.state.lightTowerCards.length === 0 &&
-            card.element === "Light"
-          ) {
-            this.setState({
-              lightTowerCards: this.state.lightTowerCards.push(card)
-            });
-          } else {
-            this.setState({
-              sideDeckCards: this.state.sideDeckCards.push(card)
-            });
-          }
-          if (
-            this.state.fireTowerCards.length === 0 &&
-            card.element === "Fire"
-          ) {
-            this.setState({
-              fireTowerCards: this.state.fireTowerCards.push(card)
-            });
-          } else {
-            this.setState({
-              sideDeckCards: this.state.sideDeckCards.push(card)
-            });
-          }
-          if (this.state.airTowerCards.length === 0 && card.element === "Air") {
-            this.setState({
-              airTowerCards: this.state.airTowerCards.push(card)
-            });
-          } else {
-            this.setState({
-              sideDeckCards: this.state.sideDeckCards.push(card)
-            });
-          }
-          if (
-            this.state.waterTowerCards.length === 0 &&
-            card.element === "Water"
-          ) {
-            this.setState({
-              waterTowerCards: this.state.waterTowerCards.push(card)
-            });
-          } else {
-            this.setState({
-              sideDeckCards: this.state.sideDeckCards.push(card)
-            });
-          }
-          if (
-            this.state.darkTowerCards.length === 0 &&
-            card.element === "Dark"
-          ) {
-            this.setState({
-              darkTowerCards: this.state.darkTowerCards.push(card)
-            });
-          } else {
-            this.setState({
-              sideDeckCards: this.state.sideDeckCards.push(card)
-            });
-          }
-        } else if (this.state.mainDeckCards.length < 60) {
-          this.setState({
-            mainDeckCards: this.state.mainDeckCards.push(card)
-          });
-        } else
-          this.setState({
-            sideDeckCards: this.state.shardDeckCards.push(card)
-          });
-      });
       console.log(this.state.championCard);
     }
 
-    //Initiate MuuriGrid object
+    //Initiate MuuriGrid objects
     this.grid = new MuuriGrid({
       node: this.gridElement,
       defaultOptions: {
@@ -191,8 +309,58 @@ export default class DeckBuilder extends Component {
         layout: { rounding: false }
       }
     });
+    this.gridChamp = new MuuriGrid({
+      node: this.gridElement2,
+      defaultOptions: {
+        dragEnabled: true,
+        layoutDuration: 75,
+        dragSortInterval: 5,
+        layout: { rounding: false }
+      }
+    });
+    this.gridSpirit = new MuuriGrid({
+      node: this.gridElement3,
+      defaultOptions: {
+        dragEnabled: true,
+        layoutDuration: 75,
+        dragSortInterval: 5,
+        layout: { rounding: false }
+      }
+    });
+    this.gridTowers = new MuuriGrid({
+      node: this.gridElement4,
+      defaultOptions: {
+        dragEnabled: true,
+        layoutDuration: 75,
+        dragSortInterval: 5,
+        layout: { rounding: false }
+      }
+    });
+    this.gridShards = new MuuriGrid({
+      node: this.gridElement5,
+      defaultOptions: {
+        dragEnabled: true,
+        layoutDuration: 75,
+        dragSortInterval: 5,
+        layout: { rounding: false }
+      }
+    });
+    this.gridSide = new MuuriGrid({
+      node: this.gridElement6,
+      defaultOptions: {
+        dragEnabled: true,
+        layoutDuration: 75,
+        dragSortInterval: 5,
+        layout: { rounding: false }
+      }
+    });
 
     this.grid.getEvent("dragEnd");
+    this.gridChamp.getEvent("dragEnd");
+    this.gridSpirit.getEvent("dragEnd");
+    this.gridTowers.getEvent("dragEnd");
+    this.gridShards.getEvent("dragEnd");
+    this.gridSide.getEvent("dragEnd");
   }
 
   removeElement() {
