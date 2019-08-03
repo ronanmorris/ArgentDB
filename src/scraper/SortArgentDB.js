5b712f1f38c21ddc22a8ff3de866ff52f1ad046d
Ronan Morris <ronanmorris11@gmail.com>
2019-08-04 09:31:38 +1000

    tippy

--- src/scraper/SortArgentDB.js
+++ src/scraper/SortArgentDB.js
@@ -1,9 +1,8 @@
 "use strict";
-import {firstBy, thenBy} from "thenby";
+import { firstBy, thenBy } from "thenby";
 
 const fs = require("fs");
 let url = "./src/CardDB.json";
-
 
 //Initial DB data to sort
 let rawdata = fs.readFileSync(url);
@@ -11,32 +10,30 @@ let cards = JSON.parse(rawdata).cards;
 
 //Function to copy the existing DB as a backup
 function backupDB() {
-    //Get date and time to use as the backup name
-    var currentdate = new Date();
-    var datetime =
-      "Backup_" +
-      currentdate.getDate() +
-      "." +
-      (currentdate.getMonth() + 1) +
-      "." +
-      currentdate.getFullYear() +
-      "." +
-      currentdate.getHours() +
-      "." +
-      currentdate.getMinutes() +
-      "." +
-      currentdate.getSeconds();
-    let backupUrl = "./src/backup/" + datetime + ".json";
-    // Backup the file
-    fs.copyFile(url, backupUrl, err => {
-      if (err) throw err;
-      console.log("----- Backup Successful \n", backupUrl);
-    });
+  //Get date and time to use as the backup name
+  var currentdate = new Date();
+  var datetime =
+    "Backup_" +
+    currentdate.getDate() +
+    "." +
+    (currentdate.getMonth() + 1) +
+    "." +
+    currentdate.getFullYear() +
+    "." +
+    currentdate.getHours() +
+    "." +
+    currentdate.getMinutes() +
+    "." +
+    currentdate.getSeconds();
+  let backupUrl = "./src/backup/" + datetime + ".json";
+  // Backup the file
+  fs.copyFile(url, backupUrl, err => {
+    if (err) throw err;
+    console.log("----- Backup Successful \n", backupUrl);
+  });
 }
 
-function sorting() {
-
-}
+function sorting() {}
 
 let err = false;
 
@@ -47,8 +44,4 @@ function runSort() {
     console.log("ERROR: Could not make backup of existing DB File. Exiting...");
     return;
   }
-  cards.sort(
-    firstBy((a, b) => )
-  )
 }
-
